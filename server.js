0b836a9878d44b9bb59335b42b54bd70e4481a47
katrina gardner <yutzeng0314@gmail.com>
2014-12-13 16:13:53 -0500

    revised2

--- server.js
+++ server.js
@@ -1,48 +1,3 @@
-// var Websocket = require("ws").Server;
-// var server = new Websocket({port:3000});
-// var clients = [];
-// var history= [];
-// server.on("connection", function(ws){
-//   clients.push(ws);
-//   clients.forEach(function(client){
-//     client.send("Client connected!");
-//   })
-//   for (i=0;i<history.length;i++){
-//     ws.send(history[i]);
-//   }
-//   var history1 = history.join("\n");
-//   ws.send(history1);
-//
-//
-//   console.log(clients.length+" clients are in the room");
-//
-//   ws.on("message",function(message){
-//     var hash=JSON.parse(message);
-//     history.push(hash.name + " : " + hash.words);
-//     console.log(hash.name + " : " + hash.words);
-//
-//     //console.log(message);
-//     for (i=0;i<clients.length;i++){
-//         clients[i].send(hash.name + " : " + hash.words);
-//       }
-//     })
-//
-//     ws.on("close",function(){
-//
-//       var x = clients.indexOf(ws);
-//       clients.splice(x,1);
-//       console.log(clients.length+" clients are  still in the room");
-//       clients.forEach(function(client)
-//       {
-//         client.send("Oh no, someone left!");
-//
-//       })
-//     })
-//   })
-
-//new code
-
-
 var wss = require("ws").Server;
 var server = new wss({port:3000});
 var clients = [];
@@ -55,7 +10,7 @@ server.on("connection", function(ws){
   }
   clients.push(ws);
   clients.forEach(function(client){
-    var servermsg = {name:"Server", newMessage:"client connected!", color: "black"};
+    var servermsg = {name:"Server", newMessage:"Someone just connected!", color: "black"};
     var J_servermsg= JSON.stringify(servermsg);
     client.send(J_servermsg);
 
@@ -86,9 +41,8 @@ server.on("connection", function(ws){
         var badarry=bad.split(" ");
 
         badarry.forEach(function(word){
-
-        if(word === "fuck" || word === "ass"){
-        var serverword = {name:"server", newMessage:"NO cursing in the chat room", color: "black"};
+        if(word === "fuck" || word === "ass" || word === "shit"){
+        var serverword = {name:"server", newMessage:"Watch your language, smartass!", color: "black"};
         var J_serverword= JSON.stringify(serverword);
         ws.send(J_serverword);
         ws.close();
